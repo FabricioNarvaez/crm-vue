285d12e558eb9b7d6f2f46828d8f5a41581ba74c
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-07-29 12:00:44 +0200

    Refactor: restructure App.vue to include header and main layout; update ClientesView heading style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-    <RouterView />
+    <header class="bg-slate-800">
+        <div class="mx-auto max-w-6xl py-10">
+            <h1 class="text-4xl font-extrabold text-white pl-10">Administrador de Clientes</h1>
+        </div>
+    </header>
+
+    <main class="max-w-6xl mx-auto mt-10 p-5">
+        <RouterView />
+    </main>
 </template>
 
 <script setup>
