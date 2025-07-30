71e1c93fa89f3ca3b6850c56807b8f9254b6231c
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-07-30 13:14:25 +0200

    Refactor: enhance RouterLink styling with text color and hover effects

--- src/components/UI/RouterLink.vue
+++ src/components/UI/RouterLink.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <RouterLink :to="{ name: to }" class="block md:inline-block rounded bg-blue-600 py-2 px-3">
+        <RouterLink :to="{ name: to }" class="block md:inline-block rounded bg-blue-600 py-2 px-3 text-white 
+        uppercase font-bold hover:bg-blue-700 text-sm shadow">
             <slot></slot>
         </RouterLink>
     </div>
