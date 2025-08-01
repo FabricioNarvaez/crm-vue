7a7b61e1249180febc071848bc8ce9c183af28b3
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-01 12:11:24 +0200

    Formkit installed

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './assets/main.css'
 import App from './App.vue'
 import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(router)
+.use(plugin, defaultConfig())
+.mount('#app')
