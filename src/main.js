a208dcf740b66a46b01952258cef9708f77dd2b6
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-01 12:31:28 +0200

    formkit/i18n installed

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import './assets/main.css'
 import App from './App.vue'
 import router from './router'
 import { plugin, defaultConfig } from '@formkit/vue'
+import { es } from '@formkit/i18n'
 
 createApp(App)
 .use(router)
-.use(plugin, defaultConfig())
+.use(plugin, defaultConfig({
+    locales: { es },
+    locale: 'es',
+  }))
 .mount('#app')
