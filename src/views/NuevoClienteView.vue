2b656f973540097efde5687cd8a04b221502e3e9
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-01 12:33:34 +0200

    Implement formkit on NuevoClienteview

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -5,12 +5,20 @@
         </RouterLink>
 
         <Heading>{{ titulo }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <FormKit type="form">
+                <FormKit type="text" label="Nombre" placeholder="Nombre de cliente" validation="required"
+                :validation-messages="{ required: 'El Nombre del cliente es obligatorio'}"/>
+            </FormKit>
+        </div>
     </div>
 </template>
 
 <script setup>
     import RouterLink from '../components/UI/RouterLink.vue';
     import Heading from '../components/UI/Heading.vue';
+    import { FormKit }  from '@formkit/vue';
 
     defineProps({
         titulo: {
