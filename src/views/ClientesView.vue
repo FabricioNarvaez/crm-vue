ef1db5ca418b6654c2ed6f6298511f40276b7fed
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-06 16:32:13 +0200

    Add API configuration and get customer data in ClientesView

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -9,12 +9,25 @@
 </template>
 
 <script setup>
+    import axios from 'axios';
     import RouterLink from '../components/UI/RouterLink.vue';
     import Heading from '../components/UI/Heading.vue';
+    import { onMounted } from 'vue';
 
     defineProps({
         titulo: {
             type: String
         }
     });
+
+    onMounted(() => {
+        const Api_URL = import.meta.env.VITE_API_URL;
+        axios.get(`${Api_URL}/clientes`)
+            .then(({ data }) => {
+                
+            })
+            .catch(error => {
+                console.error('Error al obtener los datos de clientes:', error);
+            });
+    })
 </script>
