928eb43761d2f68ba359ec65b3037e8050acffb9
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-08-07 17:45:19 +0200

    Fix color styles on client state

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -10,8 +10,8 @@
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm">
             <button class="inline-flex rounded-full px-2 text-xs font-semibold leadind-5"
-            :class="[estadoCliente ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800']">
-                {{ estadoCliente }}
+            :class="[props.cliente.estado ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800']">
+                {{ props.cliente.estado ? 'Activo' : 'Inactivo' }}
             </button>
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
@@ -32,9 +32,5 @@
 
     const nombreCompleto = computed(() => {
         return `${props.cliente.nombre} ${props.cliente.apellido}`;
-    });
-
-    const estadoCliente = computed(() => {
-        return props.cliente.estado ? 'Activo' : 'Inactivo';
     });
 </script>
